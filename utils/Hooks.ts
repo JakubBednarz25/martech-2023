2ce513984c565ea245ec7cf92754e9d855cdd4a3
Jakub Bednarz <73561350+JakubBednarz25@users.noreply.github.com>
2023-01-17 18:19:56 +0000

    changed localstorage token ting

--- utils/Hooks.ts
+++ utils/Hooks.ts
@@ -13,32 +13,20 @@ const useCart = () => {
   const [cart, setCart] = useState<CartItemType[]>([]);
 
   const setItemAmount = (itemId: string, amount: number) => {
-    console.log("1");
     let cartCopy = [...cart];
-    console.log(itemId);
-    let itemExistsInCart = cartCopy.find((i) => {
-      console.log(i);
-      return i.item.id === itemId;
-    });
-    console.log("2");
-    console.log(itemExistsInCart);
-    console.log("3");
+    let itemExistsInCart = cartCopy.find((i) => i.item.id === itemId);
     if (itemExistsInCart) {
-      console.log("4");
       if (amount === 0) {
-        console.log("5");
         //Remove from cart
         let newCart = cart.filter((i) => i.item.id !== itemId);
         setCart(newCart);
-        localStorage.setItem("cart", JSON.stringify(newCart));
+        localStorage.setItem("newCart", JSON.stringify(newCart));
       } else {
-        console.log("6");
         itemExistsInCart.amount = amount;
         setCart(cartCopy);
-        localStorage.setItem("cart", JSON.stringify(cartCopy));
+        localStorage.setItem("newCart", JSON.stringify(cartCopy));
       }
     } else {
-      console.log("7");
       let item = Items.find((i) => i.id === itemId) as ShopItem;
       let newCart = [
         ...cart,
@@ -48,15 +36,14 @@ const useCart = () => {
         },
       ];
       setCart(newCart);
-      localStorage.setItem("cart", JSON.stringify(newCart));
+      localStorage.setItem("newCart", JSON.stringify(newCart));
     }
-    console.log("8");    
   };
 
   useEffect(() => {
     setCart(
-      typeof window !== "undefined" && localStorage.getItem("cart") !== null
-        ? JSON.parse(localStorage.getItem("cart") as string)
+      typeof window !== "undefined" && localStorage.getItem("newCart") !== null
+        ? JSON.parse(localStorage.getItem("newCart") as string)
         : []
     );
   }, []);
